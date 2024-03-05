78e47433245b9c490a4a137a80e35d20ec140923
Владимир Зубко <falcluft@gmail.com>
2024-03-05 23:33:38 +0300

    Первый коммит

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+			Хочу, чтоб все, кто чего-то не понимает, поняли всё, чего не понимают.
         </p>
       </header>
     </div>
